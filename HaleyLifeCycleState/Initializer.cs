018495b207a3d6d6bf3cab19751766864bc574fa
Quetzal <rmsmech@outlook.com>
2025-11-08 15:27:27 +0400

    something happening

---
+++ HaleyLifeCycleState/Initializer.cs
@@ -0,0 +1,17 @@
+﻿using Haley.Abstractions;
+using Haley.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Haley {
+    public class Initializer {
+        const string FALLBACK_DB_NAME = "hdb_lc_state";
+        public static Task<IFeedback> InitializeAsync(IAdapterGateway agw, string adapterKey) {
+           
+
+        }
+    }
+}
